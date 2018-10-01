2ef8fb607cdfb64c629cd6d90cc586026a1046b6
sstillwell19 <43002712+sstillwell19@users.noreply.github.com>
2018-10-01 08:32:34 -0400

    Add files via upload

---
+++ src/OperationsPractice.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ * @author Max Stillwell
+ *
+ */
+public class OperationsPractice 
+{
+
+	public static void main(String[] args) 
+	{
+		int number;
+		number = 10;
+		number = number + 5;		
+		System.out.println("Add 5:" + number);
+		number = number/2;
+		System.out.println("Divide by 2:" + number);
+		number *= 4;
+		System.out.println("Multiply by 4:" +  number);
+		number -= 6;
+		System.out.println("Subtract 6:" + number);
+		number %= 2;
+		System.out.print("Modularly divide by 2:" + number);
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
