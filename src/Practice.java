89197847ab1400ecda8ce858cbae17a27bd136e7
sstillwell19 <43002712+sstillwell19@users.noreply.github.com>
2018-10-11 08:39:01 -0400

    Add files via upload

---
+++ src/Practice.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Practice 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		//print a statement
+		System.out.print("I know how to print to the screen");
+		//create an integer, double, and string variable and initialize them 	
+		int num;  //declared variable that will store ints and is called num 
+		num = 20; // assigned a value ---> first one called initialization
+		double decimal = 3.14;// declare and initialize in one line
+		String str = "this is a string"; //String must have a capital S
+		// print contents of variable 
+		System.out.println(num);
+		System.out.println(decimal + "\n");
+		System.out.println(str);
+	
+	
+	
+	
+	
+	
+	}
+	
+	
+
+}
